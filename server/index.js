62bb7e5c736fe800353a9344c320ffbe470121fd
lennon2jones <lj0039@mix.wvu.edu>
2022-11-18 12:56:22 -0500

    Added scripts to start server and client concurrently

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 //Send all menu items from DB to frontend
-app.get("/", (req, res) => {
+app.get("/menu", (req, res) => {
     /*
         Select * from menu
         Send objects
