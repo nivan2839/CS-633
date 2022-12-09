9ae5944e906af5d409d0f91cddf74f74171b90f1
lennon2jones <lj0039@mix.wvu.edu>
2022-12-08 21:51:49 -0500

    Made very basic admin page

--- admin/src/App.js
+++ admin/src/App.js
@@ -8,24 +8,23 @@ function App() {
   const [orders, setOrders] = useState([]);
   useEffect(() => {
     getOrders()
-      .then(data => setOrders(data))
+      .then(data => setOrders(data.orders))
   }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        {console.log(orders)}
+        <div style={{width: '50%'}}>
+              <h3 style={{width: '50%', float: 'left'}}>Order ID</h3>
+              <h3 style={{width: '50%', float: 'left'}}>Ordered Items</h3>
+        </div>
+        {orders.map(x => 
+          <div style={{width: '50%'}}>
+              <p style={{width: '50%', float: 'left'}}>{x.order_id}</p>
+              <p style={{width: '50%', float: 'left'}}> {x.ordered_items.map(y => `[${y.count} ${y.food_name}] `)}</p>
+          </div>)}
+        </div>
     </div>
   );
 }
